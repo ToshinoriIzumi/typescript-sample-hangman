2506feb5affef39f72680092f5be440033178cc8
izumi.toshinori <toshinori622@gmail.com>
2024-05-20 15:06:48 +0900

    Quizクラスの追加とUserInterfaceを追加

--- dist/hangman.js
+++ dist/hangman.js
@@ -1,3 +1,58 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Game Start!!!");
+const chalk_1 = __importDefault(require("chalk"));
+const figlet_1 = __importDefault(require("figlet"));
+const promises_1 = __importDefault(require("readline/promises"));
+const rl = promises_1.default.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+class Quiz {
+    questions;
+    constructor(questions) {
+        this.questions = questions;
+    }
+    hasNext() {
+        return this.questions.length > 0;
+    }
+    getNext() {
+        const idx = Math.floor(Math.random() * this.questions.length);
+        const [question] = this.questions.splice(idx, 1);
+        return question;
+    }
+    lefts() {
+        return this.questions.length;
+    }
+}
+const CLI = {
+    async input() {
+        const input = await rl.question("文字または単語を入力してください: ");
+        return input.replaceAll(" ", "").toLowerCase();
+    },
+    clear() {
+        console.clear();
+    },
+    destroy() {
+        rl.close();
+    },
+    output(message, color = "white") {
+        console.log(chalk_1.default[color](message), "\n");
+    },
+    outputAnswer(message) {
+        console.log(figlet_1.default.textSync(message, { font: "Big" }), "\n");
+    }
+};
+async function testQuestion() {
+    CLI.clear();
+    const userInput = await CLI.input();
+    CLI.output(userInput, "red");
+    CLI.outputAnswer(userInput);
+    CLI.destroy();
+}
+testQuestion();
+// const questions: Question[] = rawData;
+// const quiz = new Quiz(questions);
+// console.log("Game Start!!!");
--- src/hangman.ts
+++ src/hangman.ts
@@ -1,1 +1,74 @@
-console.log("Game Start!!!");
+import chalk from "chalk";
+import figlet from "figlet";
+import rawData from "./data/questions.test.json"
+import readlinePromise from "readline/promises";
+
+const rl = readlinePromise.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+interface Question {
+    word: string,
+    hint: string
+}
+
+class Quiz {
+    questions: Question[];
+    constructor(questions: Question[]) {
+        this.questions = questions;
+    }
+
+    hasNext(): boolean {
+        return this.questions.length > 0;
+    }
+
+    getNext(): Question {
+        const idx = Math.floor(Math.random() * this.questions.length);
+        const [question] = this.questions.splice(idx, 1);
+        return question;
+    }
+
+    lefts(): number {
+        return this.questions.length;
+    }
+}
+
+type Color = "red" | "green" | "yellow" | "white";
+
+interface UserInterface {
+    input(): Promise<string>;
+    clear(): void;
+    destroy(): void;
+    output(message: string, color?: Color): void;
+    outputAnswer(message: string): void;
+}
+
+const CLI: UserInterface = {
+    async input() {
+        const input = await rl.question("文字または単語を入力してください: ");
+        return input.replaceAll(" ", "").toLowerCase();
+    },
+    clear() {
+        console.clear();
+    },
+    destroy() {
+        rl.close();
+    },
+    output(message: string, color: Color = "white") {
+        console.log(chalk[color](message), "\n");
+    },
+    outputAnswer(message: string) {
+        console.log(figlet.textSync(message, { font: "Big" }), "\n");
+    }
+}
+
+async function testQuestion() {
+    CLI.clear();
+    const userInput = await CLI.input();
+    CLI.output(userInput, "red");
+    CLI.outputAnswer(userInput);
+    CLI.destroy();
+}
+
+testQuestion();
